2fd6953ad9e35132f2cfb3b55c666cd8ff0db8d1
daris <darisborel5@gmail.com>
2020-10-25 23:46:39 +0100

    Updates

---
+++ precache-manifest.56e976fe29d36baeed698914933924f0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3e290d4e5b5eba49b833a3135da5ed",
+    "url": "/GestionAnnonce_TO52/index.html"
+  },
+  {
+    "revision": "44567b9ab5dac38c6aac",
+    "url": "/GestionAnnonce_TO52/static/css/2.f0d63031.chunk.css"
+  },
+  {
+    "revision": "f47a19daa9af2b1a5b81",
+    "url": "/GestionAnnonce_TO52/static/css/main.ec4961d4.chunk.css"
+  },
+  {
+    "revision": "44567b9ab5dac38c6aac",
+    "url": "/GestionAnnonce_TO52/static/js/2.5c6207c5.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/GestionAnnonce_TO52/static/js/2.5c6207c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f47a19daa9af2b1a5b81",
+    "url": "/GestionAnnonce_TO52/static/js/main.44fe6acc.chunk.js"
+  },
+  {
+    "revision": "db3c836e1bfc1c5c8482",
+    "url": "/GestionAnnonce_TO52/static/js/runtime-main.72eb2586.js"
+  },
+  {
+    "revision": "c0a2b994f4739a39c4814d1807c1e4ad",
+    "url": "/GestionAnnonce_TO52/static/media/covoiturage.c0a2b994.jpg"
+  },
+  {
+    "revision": "969b3f995419a10745f9686a893cb3dd",
+    "url": "/GestionAnnonce_TO52/static/media/emploie.969b3f99.jpg"
+  },
+  {
+    "revision": "3ab1254440f32c60bec7052b1fbdbda0",
+    "url": "/GestionAnnonce_TO52/static/media/evènement.3ab12544.jpg"
+  },
+  {
+    "revision": "c575e0e939a405cb49670e07410b2058",
+    "url": "/GestionAnnonce_TO52/static/media/location.c575e0e9.jpg"
+  },
+  {
+    "revision": "7f7defe9bffd3996e947b0fdcbe25342",
+    "url": "/GestionAnnonce_TO52/static/media/stage.7f7defe9.jpg"
+  }
+]);
